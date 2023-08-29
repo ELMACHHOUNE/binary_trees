72f383cec10281552bd8d2be15ec652dd1018150
MOHAMED EL MACHHOUNE <91902222+ELMACHHOUNE@users.noreply.github.com>
2023-08-29 20:42:10 +0100

    Update 131-main.c

--- tests/131-main.c
+++ tests/131-main.c
@@ -1,68 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * heap_insert - inserts a value in Max Binary Heap
- * @root: a double pointer to the root node of the Heap to insert the value
- * @value: the value to store in the node to be inserted
+ * main - Entry point
  *
- * Return: a pointer to the created node
- *         NULL on failure
+ * Return: 0 on success, error code on failure
  */
-heap_t *heap_insert(heap_t **root, int value)
+int main(void)
 {
-	heap_t *tree, *new, *flip;
-	int size, leaves, sub, bit, level, tmp;
+	heap_t *root;
+	heap_t *node;
 
-	if (!root)
-		return (NULL);
-	if (!(*root))
-		return (*root = binary_tree_node(NULL, value));
-	tree = *root;
-	size = binary_tree_size(tree);
-	leaves = size;
-	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
-		leaves -= sub;
-	/* subtract all nodes except for bottom-most level */
-
-	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
-		tree = leaves & bit ? tree->right : tree->left;
-	/*
-	 * Traverse tree to first empty slot based on the binary
-	 * representation of the number of leaves.
-	 * Example -
-	 * If there are 12 nodes in a complete tree, there are 5 leaves on
-	 * the 4th tier of the tree. 5 is 101 in binary. 1 corresponds to
-	 * right, 0 to left.
-	 * The first empty node is 101 == RLR, *root->right->left->right
-	 */
-
-	new = binary_tree_node(tree, value);
-	leaves & 1 ? (tree->right = new) : (tree->left = new);
-
-	flip = new;
-	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
-	{
-		tmp = flip->n;
-		flip->n = flip->parent->n;
-		flip->parent->n = tmp;
-		new = new->parent;
-	}
-	/* Flip values with parent until parent value exceeds new value */
-
-	return (new);
-}
-
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
- *
- * Return: size of the tree
- *         0 if tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	root = NULL;
+	node = heap_insert(&root, 98);
+	printf("Inserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 402);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 12);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 46);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 128);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 256);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 512);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 50);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	return (0);
 }
